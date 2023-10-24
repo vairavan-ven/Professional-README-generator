52f6ffe4c82fea947ec3927a1f81adc1a2b26769
vairavan-ven <vairavan.ven@gmail.com>
2023-10-24 16:12:45 +1100

    added questions for README

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'userStory',
+        message: 'What is the User Story:'
+    },
+    {
+        type: 'input',
+        name: 'acceptanceCriteria',
+        message: 'What is the Acceptance Criteria:'
+    },
+    {
+        type: 'input',
+        name: 'deployedLink',
+        message: 'What is the deployed link of the prject:'
+    }
+        
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
